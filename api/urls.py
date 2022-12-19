9f6e62140044134e8112305f46dbea606cf32acb
aayushkc <57221458+aayushkc@users.noreply.github.com>
2022-12-20 00:26:00 +0545

    added shipping details api

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,9 @@ from . import views
 urlpatterns = [
      path('products', views.ProductList.as_view(), name='product'),
      path('cart-items', views.CartItem.as_view(), name = 'cart-item'),
-     path('add-to-cart/<int:id>', views.AddCartItems.as_view(), name='add-to-cart')
+     path('add-to-cart/<int:id>', views.AddCartItems.as_view(), name='add-to-cart'),
+     path('shipping', views.ShippingDetails.as_view(), name='shipping'),
+     
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from .serializers import *
 from .models import *
+import datetime
 
 # Create your views here.
 
@@ -24,19 +25,18 @@ class CartItem(APIView):
                 
             }
         try:
-            order = Order.objects.get(customer = user)
-            if order.complete == True:
-                return Response("No Orders")
-            orderItems = OrderItem.objects.filter(order=order)
-            context["cart-total"] = order.get_cart_total
-            context["cart-items"] = order.get_cart_items
-            for orderItem in orderItems:
-                context["data"].append( {
-                    'product-name': orderItem.product.name,
-                    'quantity': orderItem.quantity,
-                    'price': orderItem.product.price,
-                    'total':orderItem.get_total
-                })
+            orders = Order.objects.filter(customer = user, complete = False)
+            for order in orders:
+                orderItems = OrderItem.objects.filter(order=order)
+                context["cart-total"] = order.get_cart_total
+                context["cart-items"] = order.get_cart_items
+                for orderItem in orderItems:
+                    context["data"].append( {
+                        'product-name': orderItem.product.name,
+                        'quantity': orderItem.quantity,
+                        'price': orderItem.product.price,
+                        'total':orderItem.get_total
+                    })
         except:
             context["message"] = "No data available"
             
@@ -47,7 +47,7 @@ class AddCartItems(APIView):
         print(id, request.user.customer)
         customer = request.user.customer
         product = Product.objects.get(id = id)
-        order, created = Order.objects.get_or_create(customer = customer)
+        order, created = Order.objects.get_or_create(customer = customer, complete = False)
         orderItem = OrderItem.objects.create(order = order, product = product)
         serializer = OrderItemSerializer(orderItem, data= request.data)
         if serializer.is_valid():
@@ -55,4 +55,42 @@ class AddCartItems(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ShippingDetails(APIView):
+    def get(self, request):
+        customer = request.user.customer
+        orders = Order.objects.filter(customer=customer, complete = True)
+        data = []
+        for order in orders:
+            shipping = ShippingAddress.objects.filter(order=order)
+            serializer = ShippingSerializer(shipping, many = True)
+            data.append(serializer.data)
+        return Response(data)
+    def post(self, request):
+        customer = request.user.customer
+        order = Order.objects.get(customer=customer, complete = False)
+        transactoinID = datetime.datetime.now().timestamp()
+        order.transaction_id = transactoinID
+        shipping = ShippingAddress.objects.create(customer=customer, order = order)
+        serializer = ShippingSerializer(shipping, data= request.data)
+        orderItems = OrderItem.objects.filter(order= order)
+        items = []
+        for orderItem in orderItems:
+            items.append({
+                "prodcutName": orderItem.product.name,
+                "quantity": orderItem.quantity,
+                "price": orderItem.get_total
+            })
+        if serializer.is_valid():
+            order.complete = True
+            order.save()
+            serializer.save()
+            context = {
+                "transctionId": order.transaction_id,
+                "data":serializer.data,
+                "orderedItems":items,
+                "total": order.get_cart_total
+
+            }
+        return Response(context, status=status.HTTP_201_CREATED)
     
